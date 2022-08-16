dea711a032825245cf412fd3793a350f8dac4045
Iligiz <98862319+iligiz099@users.noreply.github.com>
2022-08-16 10:19:53 +0600

    /react-counter

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './index.scss';
+
+function App() {
+  const [count, setCount] = React.useState(11);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+  const onClickMinus = () => {
+    setCount(count - 1);
+  }
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
